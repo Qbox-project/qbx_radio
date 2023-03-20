233a5d5335f51af1c84d326a374ef8311f612ecd
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-03-21 00:18:11 +0100

    feat(html): allow any resource name

--- html/js/script.js
+++ html/js/script.js
@@ -11,10 +11,10 @@ $(function() {
 
     document.onkeyup = function (data) {
         if (data.which == 27) { // Escape key
-            $.post('https://qb-radio/escape', JSON.stringify({}));
+            $.post(`https://${GetParentResourceName()}/escape`, JSON.stringify({}));
             QBRadio.SlideDown()
         } else if (data.which == 13) { // Enter key
-            $.post('https://qb-radio/joinRadio', JSON.stringify({
+            $.post(`https://${GetParentResourceName()}/joinRadio`, JSON.stringify({
                 channel: $("#channel").val()
             }));
         }
@@ -26,7 +26,7 @@ QBRadio = {}
 $(document).on('click', '#submit', function(e){
     e.preventDefault();
 
-    $.post('https://qb-radio/joinRadio', JSON.stringify({
+    $.post(`https://${GetParentResourceName()}/joinRadio`, JSON.stringify({
         channel: $("#channel").val()
     }));
 });
@@ -34,13 +34,13 @@ $(document).on('click', '#submit', function(e){
 $(document).on('click', '#disconnect', function(e){
     e.preventDefault();
 
-    $.post('https://qb-radio/leaveRadio');
+    $.post(`https://${GetParentResourceName()}/leaveRadio`);
 });
 
 $(document).on('click', '#volumeUp', function(e){
     e.preventDefault();
 
-    $.post('https://qb-radio/volumeUp', JSON.stringify({
+    $.post(`https://${GetParentResourceName()}/volumeUp`, JSON.stringify({
         channel: $("#channel").val()
     }));
 });
@@ -48,7 +48,7 @@ $(document).on('click', '#volumeUp', function(e){
 $(document).on('click', '#volumeDown', function(e){
     e.preventDefault();
 
-    $.post('https://qb-radio/volumeDown', JSON.stringify({
+    $.post(`https://${GetParentResourceName()}/volumeDown`, JSON.stringify({
         channel: $("#channel").val()
     }));
 });
@@ -56,7 +56,7 @@ $(document).on('click', '#volumeDown', function(e){
 $(document).on('click', '#decreaseradiochannel', function(e){
     e.preventDefault();
 
-    $.post('https://qb-radio/decreaseradiochannel', JSON.stringify({
+    $.post(`https://${GetParentResourceName()}/decreaseradiochannel`, JSON.stringify({
         channel: $("#channel").val()
     }));
 });
@@ -64,7 +64,7 @@ $(document).on('click', '#decreaseradiochannel', function(e){
 $(document).on('click', '#increaseradiochannel', function(e){
     e.preventDefault();
 
-    $.post('https://qb-radio/increaseradiochannel', JSON.stringify({
+    $.post(`https://${GetParentResourceName()}/increaseradiochannel`, JSON.stringify({
         channel: $("#channel").val()
     }));
 });
@@ -72,7 +72,7 @@ $(document).on('click', '#increaseradiochannel', function(e){
 $(document).on('click', '#poweredOff', function(e){
     e.preventDefault();
 
-    $.post('https://qb-radio/poweredOff', JSON.stringify({
+    $.post(`https://${GetParentResourceName()}/poweredOff`, JSON.stringify({
         channel: $("#channel").val()
     }));
 });
