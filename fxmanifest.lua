51310281699fa724ca78098af944d6cdd19b6ba3
Molicheu <64894726+Molicheu@users.noreply.github.com>
2023-04-07 20:31:29 -0400

    could not find file `html/img/cursor.png`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,6 +14,6 @@ server_script 'server.lua'
 
 ui_page('html/ui.html')
 
-files {'html/ui.html', 'html/js/script.js', 'html/css/style.css', 'html/img/cursor.png', 'html/img/radio.png'}
+files {'html/ui.html', 'html/js/script.js', 'html/css/style.css', 'html/img/radio.png'}
 
 lua54 'yes'
