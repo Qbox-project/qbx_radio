f63e43939e77c98c636c52561fe8f27355600035
Manason <clanerp@gmail.com>
2023-09-28 08:57:17 -0700

    refactor: overhaul (#2)
    * refactor: overhaul
    
    * fix: lint errors

--- client.lua
+++ client.lua
@@ -1,35 +1,13 @@
-local QBCore = exports['qbx-core']:GetCoreObject()
-local PlayerData = QBCore.Functions.GetPlayerData() -- Just for resource restart (same as event handler)
 local radioMenu = false
 local onRadio = false
-local RadioChannel = 0
-local RadioVolume = 50
+local radioChannel = 0
+local radioVolume = 50
 local hasRadio = false
 local radioProp = nil
 
 --Function
-local function LoadAnimDic(dict)
-    if not HasAnimDictLoaded(dict) then
-        RequestAnimDict(dict)
-        while not HasAnimDictLoaded(dict) do
-            Wait(0)
-        end
-    end
-end
-
-local function SplitStr(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = {}
-    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
-        t[#t+1] = str
-    end
-    return t
-end
-
-local function connecttoradio(channel)
-    RadioChannel = channel
+local function connectToRadio(channel)
+    radioChannel = channel
     if onRadio then
         exports["pma-voice"]:setRadioChannel(0)
     else
@@ -37,10 +15,11 @@ local function connecttoradio(channel)
         exports["pma-voice"]:setVoiceProperty("radioEnabled", true)
     end
     exports["pma-voice"]:setRadioChannel(channel)
-    if SplitStr(tostring(channel), ".")[2] ~= nil and SplitStr(tostring(channel), ".")[2] ~= "" then
-        QBCore.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. ' MHz', 'success')
+    local subFreq = string.split(tostring(channel), '.')[2]
+    if subFreq and subFreq ~= "" then
+        exports.qbx_core:Notify(Config.messages['joined_to_radio'] ..channel.. ' MHz', 'success')
     else
-        QBCore.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. '.00 MHz', 'success')
+        exports.qbx_core:Notify(Config.messages['joined_to_radio'] ..channel.. '.00 MHz', 'success')
     end
 end
 
@@ -50,23 +29,23 @@ end
 
 local function leaveradio()
     closeEvent()
-    RadioChannel = 0
+    radioChannel = 0
     onRadio = false
     exports["pma-voice"]:setRadioChannel(0)
     exports["pma-voice"]:setVoiceProperty("radioEnabled", false)
-    QBCore.Functions.Notify(Config.messages['you_leave'] , 'error')
+    exports.qbx_core:Notify(Config.messages['you_leave'] , 'error')
 end
 
 local function toggleRadioAnimation(pState)
-	LoadAnimDic("cellphone@")
+    lib.requestAnimDict('cellphone@')
 	if pState then
 		TriggerEvent("attachItemRadio","radio01")
-		TaskPlayAnim(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
+		TaskPlayAnim(cache.ped, "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
 		radioProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, 1, 1, 0)
-		AttachEntityToEntity(radioProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, 1, 0, 0, 0, 2, 1)
+		AttachEntityToEntity(radioProp, cache.ped, GetPedBoneIndex(cache.ped, 57005), 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, 1, 0, 0, 0, 2, 1)
 	else
-		StopAnimTask(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 1.0)
-		ClearPedTasks(PlayerPedId())
+		StopAnimTask(cache.ped, "cellphone@", "cellphone_text_read_base", 1.0)
+		ClearPedTasks(cache.ped)
 		if radioProp ~= 0 then
 			DeleteObject(radioProp)
 			radioProp = 0
@@ -86,53 +65,39 @@ local function toggleRadio(toggle)
     end
 end
 
-local function IsRadioOn()
+local function isRadioOn()
     return onRadio
 end
 
-local function DoRadioCheck(PlayerItems)
-    local _hasRadio = false
-
-    for _, item in pairs(PlayerItems) do
-        if item.name == "radio" then
-            _hasRadio = true
-            break;
-        end
-    end
-
-    hasRadio = _hasRadio
+local function doRadioCheck()
+    hasRadio = exports.ox_inventory:Search('count', 'radio') > 0
 end
 
 --Exports
-exports("IsRadioOn", IsRadioOn)
+exports("IsRadioOn", isRadioOn)
 
 --Events
 
 -- Handles state right when the player selects their character and location.
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    PlayerData = QBCore.Functions.GetPlayerData()
-    DoRadioCheck(PlayerData.items)
+    doRadioCheck()
 end)
 
 -- Resets state on logout, in case of character change.
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
-    DoRadioCheck({})
-    PlayerData = {}
+    hasRadio = false
     leaveradio()
 end)
 
--- Handles state when PlayerData is changed. We're just looking for inventory updates.
-RegisterNetEvent('QBCore:Player:SetPlayerData', function(val)
-    PlayerData = val
-    DoRadioCheck(PlayerData.items)
+AddEventHandler('ox_inventory:itemCount', function(itemName, totalCount)
+    if itemName ~= 'radio' then return end
+    hasRadio = totalCount > 0
 end)
 
 -- Handles state if resource is restarted live.
 AddEventHandler('onResourceStart', function(resource)
-    if GetCurrentResourceName() == resource then
-        PlayerData = QBCore.Functions.GetPlayerData()
-        DoRadioCheck(PlayerData.items)
-    end
+    if GetCurrentResourceName() ~= resource then return end
+    doRadioCheck()
 end)
 
 RegisterNetEvent('qb-radio:use', function()
@@ -140,7 +105,7 @@ RegisterNetEvent('qb-radio:use', function()
 end)
 
 RegisterNetEvent('qb-radio:onRadioDrop', function()
-    if RadioChannel ~= 0 then
+    if radioChannel ~= 0 then
         leaveradio()
     end
 end)
@@ -148,33 +113,36 @@ end)
 -- NUI
 RegisterNUICallback('joinRadio', function(data, cb)
     local rchannel = tonumber(data.channel)
-    if rchannel ~= nil then
-        if rchannel <= Config.MaxFrequency and rchannel ~= 0 then
-            if rchannel ~= RadioChannel then
-                if Config.RestrictedChannels[rchannel] ~= nil then
-                    if Config.RestrictedChannels[rchannel][PlayerData.job.name] and PlayerData.job.onduty then
-                        connecttoradio(rchannel)
-                    else
-                        QBCore.Functions.Notify(Config.messages['restricted_channel_error'], 'error')
-                    end
-                else
-                    connecttoradio(rchannel)
-                end
-            else
-                QBCore.Functions.Notify(Config.messages['you_on_radio'] , 'error')
-            end
-        else
-            QBCore.Functions.Notify(Config.messages['invalid_radio'] , 'error')
-        end
-    else
-        QBCore.Functions.Notify(Config.messages['invalid_radio'] , 'error')
+    if not rchannel then
+        exports.qbx_core:Notify(Config.messages['invalid_radio'] , 'error')
+        cb("ok")
+        return
     end
-    cb("ok")
+
+    if rchannel > Config.MaxFrequency or rchannel == 0 then
+        exports.qbx_core:Notify(Config.messages['invalid_radio'] , 'error')
+        cb("ok")
+        return
+    end
+
+    if rchannel == radioChannel then
+        exports.qbx_core:Notify(Config.messages['you_on_radio'] , 'error')
+        cb("ok")
+        return
+    end
+
+    if Config.RestrictedChannels[rchannel] and not Config.RestrictedChannels[rchannel][QBX.PlayerData.job.name] or not QBX.PlayerData.job.onduty then
+        exports.qbx_core:Notify(Config.messages['restricted_channel_error'], 'error')
+        cb("ok")
+        return
+    end
+
+    connectToRadio(rchannel)
 end)
 
 RegisterNUICallback('leaveRadio', function(_, cb)
-    if RadioChannel == 0 then
-        QBCore.Functions.Notify(Config.messages['not_on_radio'], 'error')
+    if radioChannel == 0 then
+        exports.qbx_core:Notify(Config.messages['not_on_radio'], 'error')
     else
         leaveradio()
     end
@@ -182,40 +150,40 @@ RegisterNUICallback('leaveRadio', function(_, cb)
 end)
 
 RegisterNUICallback("volumeUp", function(_, cb)
-	if RadioVolume <= 95 then
-		RadioVolume = RadioVolume + 5
-		QBCore.Functions.Notify(Config.messages["volume_radio"] .. RadioVolume, "success")
-		exports["pma-voice"]:setRadioVolume(RadioVolume)
+	if radioVolume <= 95 then
+		radioVolume = radioVolume + 5
+		exports.qbx_core:Notify(Config.messages["volume_radio"] .. radioVolume, "success")
+		exports["pma-voice"]:setRadioVolume(radioVolume)
 	else
-		QBCore.Functions.Notify(Config.messages["decrease_radio_volume"], "error")
+		exports.qbx_core:Notify(Config.messages["decrease_radio_volume"], "error")
 	end
     cb('ok')
 end)
 
 RegisterNUICallback("volumeDown", function(_, cb)
-	if RadioVolume >= 10 then
-		RadioVolume = RadioVolume - 5
-		QBCore.Functions.Notify(Config.messages["volume_radio"] .. RadioVolume, "success")
-		exports["pma-voice"]:setRadioVolume(RadioVolume)
+	if radioVolume >= 10 then
+		radioVolume = radioVolume - 5
+		exports.qbx_core:Notify(Config.messages["volume_radio"] .. radioVolume, "success")
+		exports["pma-voice"]:setRadioVolume(radioVolume)
 	else
-		QBCore.Functions.Notify(Config.messages["increase_radio_volume"], "error")
+		exports.qbx_core:Notify(Config.messages["increase_radio_volume"], "error")
 	end
     cb('ok')
 end)
 
 RegisterNUICallback("increaseradiochannel", function(_, cb)
-    local newChannel = RadioChannel + 1
+    local newChannel = radioChannel + 1
     exports["pma-voice"]:setRadioChannel(newChannel)
-    QBCore.Functions.Notify(Config.messages["increase_decrease_radio_channel"] .. newChannel, "success")
+    exports.qbx_core:Notify(Config.messages["increase_decrease_radio_channel"] .. newChannel, "success")
     cb("ok")
 end)
 
 RegisterNUICallback("decreaseradiochannel", function(_, cb)
     if not onRadio then return end
-    local newChannel = RadioChannel - 1
+    local newChannel = radioChannel - 1
     if newChannel >= 1 then
         exports["pma-voice"]:setRadioChannel(newChannel)
-        QBCore.Functions.Notify(Config.messages["increase_decrease_radio_channel"] .. newChannel, "success")
+        exports.qbx_core:Notify(Config.messages["increase_decrease_radio_channel"] .. newChannel, "success")
         cb("ok")
     end
 end)
@@ -235,8 +203,8 @@ CreateThread(function()
     while true do
         Wait(1000)
         if LocalPlayer.state.isLoggedIn and onRadio then
-            if not hasRadio or PlayerData.metadata.isdead or PlayerData.metadata.inlaststand then
-                if RadioChannel ~= 0 then
+            if not hasRadio or QBX.PlayerData.metadata.isdead or QBX.PlayerData.metadata.inlaststand then
+                if radioChannel ~= 0 then
                     leaveradio()
                 end
             end
