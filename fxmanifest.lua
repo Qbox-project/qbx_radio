08df0507d56db0935c325773c4e26385c463c58a
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 02:27:55 -0500

    fix: import modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,25 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-Radio'
+description 'QBX_Radio'
 repository 'https://github.com/Qbox-project/qbx_radio'
 version '1.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
-    '@qbx_core/import.lua',
+    '@qbx_core/modules/utils.lua',
     '@qbx_core/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
     'config.lua'
 }
 
-client_script 'client.lua'
+client_scripts {
+    '@qbx_core/modules/playerdata.lua',
+    'client.lua'
+}
 
 server_script 'server.lua'
-
-modules {
-    'qbx_core:playerdata',
-    'qbx_core:utils',
-}
 
 ui_page('html/ui.html')
 
