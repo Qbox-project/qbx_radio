f5f4546f07e7f78f1e23dfa7526b6477a9a6d15a
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-22 02:22:34 -0500

    fix: missed

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-local config = require 'config.client'
+local config = require 'config.shared'
 local radioMenu = false
 local onRadio = false
 local radioChannel = 0
--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,12 @@
+local config = require 'config.shared'
+
 exports.qbx_core:CreateUseableItem('radio', function(source)
     TriggerClientEvent('qbx_radio:client:use', source)
 end)
 
-for channel, config in pairs(config.restrictedChannels) do
+for channel, jobs in pairs(config.restrictedChannels) do
     exports['pma-voice']:addChannelCheck(channel, function(source)
         local player = exports.qbx_core:GetPlayer(source)
-        return config[player.PlayerData.job.name] and player.PlayerData.job.onduty
+        return jobs[player.PlayerData.job.name] and player.PlayerData.job.onduty
     end)
 end
