bb287fda330e90218021e75b0cda62599877b36a
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-12-20 01:12:20 +0100

    fix typo and add fr.lua (#14)
    * fix typo and add fr.lua

--- locales/en.lua
+++ locales/en.lua
@@ -2,11 +2,11 @@ local Translations = {
     not_on_channel = 'You\'re not connected to a channel',
     on_channel = 'You\'re already connected to this channel',
     joined_radio = 'You\'re connected to: ',
-    restricted_channel = 'You can not connect to this channel',
+    restricted_channel = 'You cannot connect to this channel',
     invalid_channel = 'This frequency is not available',
     left_channel = 'You left the channel',
     min_volume = 'The radio is already set to the lowest volume',
-    max_volume = 'he radio is already set to maximum volume',
+    max_volume = 'The radio is already set to maximum volume',
     new_volume = 'New volume: ',
     new_channel = 'New channel: '
 }
