3225ad8f924f6498b9a5b50f603261dc8da2fafc
1Evolve <155191875+1Evolve@users.noreply.github.com>
2024-01-30 00:42:43 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -4,9 +4,9 @@ exports.qbx_core:CreateUseableItem('radio', function(source)
     TriggerClientEvent('qbx_radio:client:use', source)
 end)
 
-for channel, jobs in pairs(config.restrictedChannels) do
+for channel = 1, 11 do
     exports['pma-voice']:addChannelCheck(channel, function(source)
         local player = exports.qbx_core:GetPlayer(source)
-        return jobs[player.PlayerData.job.name] and player.PlayerData.job.onduty
+        return (player.PlayerData.job.type == 'leo' or player.PlayerData.job.type == 'ems') and player.PlayerData.job.onduty
     end)
 end
